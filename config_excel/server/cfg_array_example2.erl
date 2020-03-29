c4cbe962b038e5b9358ac2ec31e7407f62df4bba
SisMaker <1713699517@qq.com>
2020-03-29 11:35:57 +0800

    erlang导出配置 字符串改成binary

--- config_excel/server/cfg_array_example2.erl
+++ config_excel/server/cfg_array_example2.erl
@@ -10,7 +10,7 @@
 get(1) ->
     #cfg_array_example2{
     'id' = 1
-    , 'name' = "examp1001"
+    , 'name' = <<"examp1001"/utf8>>
     , 'material' = [#{'res' => 1, 'id' => 10001, 'count' => 1}, #{'res' => 1, 'id' => 10001, 'count' => 1}]
     , 'attr' = [[1, 700], [2, 800], [3, 900], #{'add' => [4, 100]}]
     , 'award1' = {1, 2, 3}
@@ -20,7 +20,7 @@ get(1) ->
 get(2) ->
     #cfg_array_example2{
     'id' = 2
-    , 'name' = "examp1002"
+    , 'name' = <<"examp1002"/utf8>>
     , 'material' = #{'res' => 1, 'id' => 10001, 'count' => 1}
     , 'attr' = [[1, 700], [2, 800], [3, 901], #{'add' => [5, 100]}]
     , 'award1' = {1, 2, 3}
@@ -30,7 +30,7 @@ get(2) ->
 get(3) ->
     #cfg_array_example2{
     'id' = 3
-    , 'name' = "examp1003"
+    , 'name' = <<"examp1003"/utf8>>
     , 'material' = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
     , 'attr' = [[1, 700], [2, 800], [3, 902], #{'add' => [6, 100]}]
     , 'award1' = {1, 2, 3}
@@ -40,8 +40,8 @@ get(3) ->
 get(4) ->
     #cfg_array_example2{
     'id' = 4
-    , 'name' = "examp1004"
-    , 'material' = ["测试数据", "测试数据2", "测试数据3"]
+    , 'name' = <<"examp1004"/utf8>>
+    , 'material' = [<<"测试数据"/utf8>>, <<"测试数据2"/utf8>>, <<"测试数据3"/utf8>>]
     , 'attr' = [[1, 700], [2, 800], [3, 903], #{'add' => [7, 100]}]
     , 'award1' = {1, 2, 3}
     , 'award2' = #{'id' => 1, 'type' => 2, 'num' => 3}
@@ -50,8 +50,8 @@ get(4) ->
 get(5) ->
     #cfg_array_example2{
     'id' = 5
-    , 'name' = "examp1004"
-    , 'material' = ["test"]
+    , 'name' = <<"examp1004"/utf8>>
+    , 'material' = [<<"test"/utf8>>]
     , 'attr' = [[1, 700], [2, 800], [3, 903], #{'add' => [7, 100]}]
     , 'award1' = {1, 2, 3}
     , 'award2' = #{'id' => 1, 'type' => 2, 'num' => 3}
